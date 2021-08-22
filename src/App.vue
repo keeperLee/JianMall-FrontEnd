d08b143482eeaef3910e549a55804c0cd5e5249d
lijian <ljhhh@qq.com>
2021-08-22 13:46:42 +0800

    使用jsonp进行前端跨域处理

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,23 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
-
+// import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
   mounted() {
-    let url = "http://127.0.0.1:8083/user/sayHello";
-    axios.get(url).then((res)=>{
-        console.log(res,"res..........")
+    // let url = "http://127.0.0.1:8083/user/sayHello";
+    // axios.get(url).then((res)=>{
+    //     console.log(res,"res..........")
+    // })
+
+    let url = "https://www.imooc.com/activity/servicetime";
+    jsonp(url,(err,res)=>{
+      let result = res;
+      this.data = result;
     })
   }
 }
