3b92e4a103a40bf9a954e39b07fac0c8a630d265
lijian <ljhhh@qq.com>
2021-09-07 22:29:44 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+//根据前端的跨域方式做调整
+axios.defaults.baseURL = '';
+axios.defaults.timeout = 8000;
+//接口错误拦截
+axios.interceptors.response.use(function (response){
+    let res = response.data;
+    if(res.status == 0){
+      return res.data;
+    }else if(res.status == 10){
+      window.location.href = '/#/login'
+    }else {
+      alert(res.msg);
+    }
+})
+
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
-Vue.prototype.axios = axios;
 new Vue({
   router,
   render: h => h(App),
