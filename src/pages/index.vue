123d252b9722c6ff0b2087303b0f6d779d704a9e
lijian <ljhhh@qq.com>
2021-09-16 21:20:53 +0800

    首页轮播菜单的整体实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,16 @@
           <ul class="menu-wrap">
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <ul v-for="(item,index) in menuList" :key="index">
+                  <li v-for="(sub,idx) in item" :key="idx" >
+                    <a :href="sub ? '/#/product/' + sub.id : ''">
+                      <img :src="sub.img || '/imgs/item-box-1.png'" alt="">
+                      {{sub ? sub.name : '小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电视 盒子</a>
@@ -22,14 +32,14 @@
             <li class="menu-item">
               <a href="javascript:;">生活 箱包</a>
             </li>
-        </ul>
+          </ul>
         </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item,index) in slideList" :key="index">
             <a :href="'/#/product/'+item.id"><img :src="item.img" alt=""></a>
           </swiper-slide>
           <!-- Optional controls -->
-          <div class="swiper-pagination"  slot="pagination"></div>
+          <div class="swiper-pagination" slot="pagination"></div>
           <div class="swiper-button-prev" slot="button-prev"></div>
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
@@ -44,52 +54,82 @@
 
 <script>
 import ServiceBar from "../components/ServiceBar";
-import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import {swiper, swiperSlide} from 'vue-awesome-swiper';
 import 'swiper/dist/css/swiper.css'
 
 export default {
   name: "index",
-  components: {ServiceBar,swiper,swiperSlide},
-  data(){
+  components: {ServiceBar, swiper, swiperSlide},
+  data() {
     return {
-      swiperOption:{
-        autoplay:true,
-        loop:true,
-        effect:'cube',
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        effect: 'cube',
         cubeEffect: {
           shadowOffset: 100,
           shadowScale: 0.6
         },
         pagination: {
           el: '.swiper-pagination',
-          clickable:true
+          clickable: true
         },
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         }
       },
-      slideList:[
+      slideList: [
         {
-          id:'42',
+          id: '42',
           img: '/imgs/slider/slide-1.jpg'
         },
         {
-          id:'45',
+          id: '45',
           img: '/imgs/slider/slide-2.jpg'
         },
         {
-          id:'46',
+          id: '46',
           img: '/imgs/slider/slide-3.jpg'
         },
         {
-          id:'',
+          id: '',
           img: '/imgs/slider/slide-4.jpg'
         },
         {
-          id:'',
+          id: '',
           img: '/imgs/slider/slide-1.jpg'
         },
+      ],
+      menuList: [
+        [
+            {
+              id: 30,
+              img:'/imgs/item-box-1.png',
+              name:'小米CC9'
+            },
+          {
+            id: 31,
+            img:'/imgs/item-box-2.png',
+            name:'小米CC9'
+          },
+          {
+            id: 32,
+            img:'/imgs/item-box-3.jpg',
+            name:'小米CC9'
+          },
+          {
+            id: 33,
+            img:'/imgs/item-box-4.jpg',
+            name:'小米CC9'
+          },
+
+        ],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
       ]
     }
   }
@@ -97,53 +137,95 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  @import './../assets/scss/mixin.scss';
-  @import './../assets/scss/config.scss';
-  .index{
-    .swiper-box{
-      .nav-menu{
-        position:absolute;
-        width: 264px;
-        height: 451px;
-        z-index: 9;
-        padding:26px;
-        background-color: #55585A7A;
-        box-sizing: border-box;
-        .menu-wrap{
-          .menu-item{
-            height: 50px;
-            line-height: 50px;
-            a{
-              position: relative;
-              font-size: 16px;
-              display: block;
-              color: #ffffff;
-              padding-left: 30px;
-              &:after{
-                position: absolute;
-                right: 30px;
-                top:17.5px;
-                content: ' ';
-                @include bgImg(10px, 15px,'/imgs/icon-arrow.png');
+@import './../assets/scss/mixin.scss';
+@import './../assets/scss/config.scss';
+
+.index {
+  .swiper-box {
+    .nav-menu {
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px;
+      background-color: #55585A7A;
+      box-sizing: border-box;
+
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            position: relative;
+            font-size: 16px;
+            display: block;
+            color: #ffffff;
+            padding-left: 30px;
+
+            &:after {
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: ' ';
+              @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+            }
+          }
+          .children{
+            display: none;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top:0;
+            left :264px;
+            border:1px solid $colorH;
+            ul{
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li{
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+              }
+              a{
+                color:$colorB;
+                font-size:14px;
+              }
+              img{
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
               }
             }
-            &:hover{
-              background-color: $colorA;
+          }
+
+
+          &:hover {
+            background-color: $colorA;
+            .children{
+              display: block;
             }
           }
         }
       }
-      .swiper-container{
-        height: 451px;
-        .swiper-button-prev{
-          left:274px;
-        }
-        img{
-          width: 100%;
-          height: 100%;
-        }
+    }
+
+    .swiper-container {
+      height: 451px;
+
+      .swiper-button-prev {
+        left: 274px;
+      }
+
+      img {
+        width: 100%;
+        height: 100%;
       }
     }
   }
+}
 </style>
 
